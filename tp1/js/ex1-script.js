5b61ea674c676b666d75f470d6de367828cab3e8
Alexis DRAI <drai.alexis@gmail.com>
2020-01-05 16:54:43 +0100

    Reworked the folder tree

--- tp1/js/ex1-script.js
+++ tp1/js/ex1-script.js
@@ -1,37 +1,38 @@
-let conteneur = document.getElementById("conteneur");
+let conteneur = document.getElementById("container");
 conteneur.innerHTML = "<h3>Bienvenue !<br></h3>";
 
-
 /*------------------------boite de dialogue---------------------------*/
+
 let question = prompt("Quel est votre prénom?");
-if (question.length >= 1) {
+if (question.length > 1) {
     conteneur.innerHTML = "<h3>Bienvenue " + question + " !</h3>";
 }
 else {
-    conteneur.innerHTML = "<h3>Bienvenue mon ami ! </h3>";
+    conteneur.innerHTML = "<h3>Bienvenue mon ami !</h3>";
 }
 let body = document.querySelectorAll("body");
 body[0].style.backgroundColor = "#bbbbff";
+
 /*-----------------------Question 2-------------------------------*/
+
 let question2 = "";
 while (question2 !== "4" && question2 !== "quatre") {
     question2 = prompt("Combien font deux fois deux?");
     switch (question2) {
         case 4:
-            conteneur.innerHTML = "<br> bravo";
             break;
         case "quatre":
-            conteneur.innerHTML = "<br> bravo";
             break;
     }
+    body[0].style.backgroundColor = "#9999ff";
 }
-body[0].style.backgroundColor = "#9999ff";
 
+/*-----------------------Question 3-------------------------------*/
 
 let question3 = confirm("Voulez-vous continuer?");
-if (question3 === true) {
-    conteneur.innerHTML = "<h3 >Bienvenue !</h3><p><a href=../exercice_2/exercice2.html>Vers Exercice 2</a></p>";
+if (question3) {
+    conteneur.innerHTML = "<h3 >Bienvenue !</h3><p><a href=../exercice2.html>Vers Exercice 2</a></p>";
 }
 else {
-    conteneur.innerHTML = "<h3 >Bienvenue !</h3><p><a href=exercice1.html>On recommence !!!</a></p>";
+    conteneur.innerHTML = "<h3 >Bienvenue !</h3><p><a href=../exercice1.html>On recommence !!!</a></p>";
 }
---
+++ tp1/js/ex2-script.js
@@ -0,0 +1,62 @@
+/*------------------------boite de dialogue---------------------------*/
+let nb = Number(prompt("Entrez un chiffre(simple, cube et carré) : "));
+
+/*------------------------tableau---------------------------*/
+
+function makeTable(userInput) {
+    for (let i = 1; i <= 3; i++) {
+        if (isNaN(userInput)) {
+            alert("veuillez saisir un chiffre");
+            nb = Number(prompt("Entrez un chiffre (simple, cube, carré): "));
+            makeTable(nb);
+
+        }
+        let entry = "<td>" + Math.pow(nb, i) + "</td>";
+        document.getElementById("myTable").innerHTML += entry;
+    }
+}
+
+makeTable(nb);
+
+//////////////////////////////////////////////////////////////////
+
+function carre(num) {
+    Math.pow(num, 2);
+}
+function cube(num) {
+    Math.pow(num, 3);
+}
+
+//////////////////////////////////////////////////////////////////
+
+let horsTaxe = Number(prompt("Entrez un chiffre (TVA): "));
+
+function tva(nbActuel) {
+    return (nbActuel * 20) / 100;
+}
+
+function fillParagraph(montantHorsTaxe) {
+    if (isNaN(montantHorsTaxe)) {
+        alert("veuillez saisir un chiffre");
+        horsTaxe = Number(prompt("Entrez un chiffre (TVA): "));
+        fillParagraph(horsTaxe);
+    }
+
+    document.getElementById("ht").innerText = horsTaxe;
+
+    let toutesTaxesComprises = horsTaxe + tva(horsTaxe);
+    document.getElementById("ttc").innerText = toutesTaxesComprises;
+}
+
+fillParagraph(horsTaxe);
+let isContinue = confirm("Voulez-vous continuer?");
+
+let filePath = "";
+
+if (isContinue) {
+    filePath = "../exercice_3/exercice3.html";
+} else {
+    filePath = "../exercice_1/exercice1.html";
+}
+
+document.getElementById("links").setAttribute("href", filePath);
